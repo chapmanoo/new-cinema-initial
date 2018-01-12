3149fc8f6ec1a0f6e09f61c2e9da4ec2c89a7b6f
chapmanoo <chapmanoo@hotmail.co.uk>
2018-01-12 11:25:28 +0000

    First commit, database persistence with script for intitial data

---
+++ src/main/java/com/qa/cinema/persistence/Movie.java
@@ -0,0 +1,60 @@
+package com.qa.cinema.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(length = 100)
+	private String title;
+	
+	@Column(length = 50)
+	private String genre;
+	
+	@Column(length = 4, name = "age_rating")
+	private String ageRating;
+	
+
+	public Movie(String title, String genre, String ageRating) {
+		super();
+		this.title = title;
+		this.genre = genre;
+		this.ageRating = ageRating;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getAgeRating() {
+		return ageRating;
+	}
+
+	public void setAgeRating(String ageRating) {
+		this.ageRating = ageRating;
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Movie (id, age_Rating, genre, title) VALUES (1, '12', 'Adventure', 'Harry Potter');
+INSERT INTO Movie (id, age_Rating, genre, title) VALUES (2, '18', 'Horror', 'IT');
+INSERT INTO Movie (id, age_Rating, genre, title) VALUES (3, '15', 'Action', 'Robocop');
